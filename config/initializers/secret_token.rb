96e3c721fc3065d1d61b2d09877fb38f92a022a0
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-10-05 19:45:50 -0700

    started new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BowlTracker::Application.config.secret_key_base = 'e5d5a630df3b6dc08d2fd17a1efa08d58239088b9643bdebadf11c692430386ebd08f23f11a09b598b91fa860637d57b1e1e968ffd732dec7ec50c3638309218'
